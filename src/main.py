78f388eab35af06dce9164048b7ba8147cd4fd27
Arthur Novikov <a.novikov@xsolla.com>
2025-06-11 17:22:01 +0400

    Fixed imports

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtGui import QFontDatabase
 from PyQt6.QtWidgets import QApplication
-from app.main_window import MainWindow
+from src.app.main_window import MainWindow
 from concurrent.futures import ThreadPoolExecutor
 import sys
 
